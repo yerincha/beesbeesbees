94bedf792097c703570fbd38b5b8f514ffa38e9f
Yerin Cha <lenacha32@gmail.com>
2019-04-15 12:07:05 -0700

    Completed Beebeebee

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,17 @@
 var RetiredForagerBee = function() {
+    ForagerBee.call(this);
+    this.age = 40;
+    this.color = 'grey';
+    this.job = 'gamble';
+    this.canFly = false;
+    this.treasureChest = [];
 };
 
+RetiredForagerBee.prototype = Object.create(ForagerBee.prototype);
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
+RetiredForagerBee.prototype.forage = function () {
+    return "I am too old, let me play cards instead"
+};
+RetiredForagerBee.prototype.gamble = function () {
+    this.treasureChest.push('treasure')
+};
